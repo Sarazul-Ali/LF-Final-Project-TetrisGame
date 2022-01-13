4ff4c09df4c2235720932238922a6966289403fb
Md Sarazul Ali <55292156+Sarazul-Ali@users.noreply.github.com>
2022-01-13 20:51:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -281,7 +281,78 @@ Piece.prototype.moveLeft = function() {
     }
 }
 
+// rotate the piece
+Piece.prototype.rotate = function() {
+    let nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
+    let kick = 0;
 
+    if (this.collision(0, 0, nextPattern)) {
+        if (this.x > COL / 2) {
+            // it's the right wall
+            kick = -1; // we need to move the piece to the left
+        } else {
+            // it's the left wall
+            kick = 1; // we need to move the piece to the right
+        }
+    }
+
+    if (!this.collision(kick, 0, nextPattern)) {
+        this.unDraw();
+        this.x += kick;
+        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length; // (0+1)%4 => 1
+        this.activeTetromino = this.tetromino[this.tetrominoN];
+        this.draw();
+    }
+}
+
+let score = 0;
+
+Piece.prototype.lock = function() {
+    for (r = 0; r < this.activeTetromino.length; r++) {
+        for (c = 0; c < this.activeTetromino.length; c++) {
+            // we skip the vacant squares
+            if (!this.activeTetromino[r][c]) {
+                continue;
+            }
+            // pieces to lock on top = game over
+            if (this.y + r < 0) {
+                alert("Game Over");
+                // stop request animation frame
+                gameOver = true;
+                break;
+            }
+            // we lock the piece
+            board[this.y + r][this.x + c] = this.color;
+        }
+    }
+    // remove full rows
+    for (r = 0; r < ROW; r++) {
+        let isRowFull = true;
+        for (c = 0; c < COL; c++) {
+            isRowFull = isRowFull && (board[r][c] != VACANT);
+        }
+        if (isRowFull) {
+            // if the row is full
+            // we move down all the rows above it
+            for (y = r; y > 1; y--) {
+                for (c = 0; c < COL; c++) {
+                    board[y][c] = board[y - 1][c];
+                }
+            }
+            // the top row board[0][..] has no row above it
+            for (c = 0; c < COL; c++) {
+                board[0][c] = VACANT;
+            }
+            // increment the score
+            score += 10;
+        }
+    }
+    // update the board
+    drawBoard();
+
+    // update the score
+    scoreElement.innerHTML = score;
+}
 
 // collision fucntion
 
@@ -313,6 +384,24 @@ Piece.prototype.collision = function(x, y, piece) {
     return false;
 }
 
+// CONTROL the piece
+
+document.addEventListener("keydown", CONTROL);
+
+function CONTROL(event) {
+    if (event.keyCode == 37) {
+        p.moveLeft();
+        dropStart = Date.now();
+    } else if (event.keyCode == 38) {
+        p.rotate();
+        dropStart = Date.now();
+    } else if (event.keyCode == 39) {
+        p.moveRight();
+        dropStart = Date.now();
+    } else if (event.keyCode == 40) {
+        p.moveDown();
+    }
+}
 
 // drop the piece every 1sec
 
